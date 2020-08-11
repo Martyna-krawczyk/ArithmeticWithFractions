a2ca0d4da5746b7c70564a858da93f233535d4ca
Martyna-Krawczyk <martynakraw@gmail.com>
2020-08-11 17:41:40 +1000

    non-zero plus zero

--- ArithmeticWithFractions/Fraction.cs
+++ ArithmeticWithFractions/Fraction.cs
@@ -3,8 +3,21 @@ namespace ArithmeticWithFractions
 {
     public class Fraction
     {
-        public Fraction()
+        private int _integerValue { get; set; }
+
+        public Fraction(int integerValue)
         {
+            _integerValue = integerValue;
+        }
+
+        public Fraction Plus(Fraction fraction)
+        {
+            return this;
+        }
+
+        public int IntValue()
+        {
+            return _integerValue;
         }
     }
 }
--- ArithmeticWithFractionsTests/AddFractionsTests.cs
+++ ArithmeticWithFractionsTests/AddFractionsTests.cs
@@ -1,14 +1,26 @@
 using System;
+using ArithmeticWithFractions;
 using Xunit;
 
 namespace ArithmeticWithFractionsTests
 {
-    public class UnitTest1
+    public class AddFractionsTests
     {
         [Fact]
-        public void Test1()
+        public void ZeroPlusZero()
         {
+            var sum = new Fraction(0).Plus(new Fraction(0));
 
+            Assert.Equal(0, sum.IntValue());
         }
+
+        [Fact]
+        public void NonZeroPlusZero()
+        {
+            var sum = new Fraction(3).Plus(new Fraction(0));
+
+            Assert.Equal(3, sum.IntValue());
+        }
+
     }
 }
