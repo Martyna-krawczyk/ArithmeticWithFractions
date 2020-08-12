9a0475c50742893b2d7dbb56afd9b30e72683e45
Martyna-Krawczyk <martynakraw@gmail.com>
2020-08-12 12:48:59 +1000

    Shortened tests by inlining temporary variables.

--- ArithmeticWithFractionsTests/AddFractionsTests.cs
+++ ArithmeticWithFractionsTests/AddFractionsTests.cs
@@ -9,42 +9,31 @@ namespace ArithmeticWithFractionsTests
         [Fact]
         public void ZeroPlusZero()
         {
-            var sum = new Fraction(0).Plus(new Fraction(0));
-
-            Assert.Equal(0, sum.IntValue());
+            Assert.Equal(0, new Fraction(0).Plus(new Fraction(0)).IntValue());
         }
 
         [Fact]
         public void NonZeroPlusZero()
         {
-            var sum = new Fraction(3).Plus(new Fraction(0));
-
-            Assert.Equal(3, sum.IntValue());
+            Assert.Equal(3, new Fraction(3).Plus(new Fraction(0)).IntValue());
         }
         
         [Fact]
         public void ZeroPlusNonZero()
         {
-            var sum = new Fraction(0).Plus(new Fraction(5));
-
-            Assert.Equal(5, sum.IntValue());
+            Assert.Equal(5, new Fraction(0).Plus(new Fraction(5)).IntValue());
         }
         
         [Fact]
         public void NonNegativeNonZeroOperands()
         {
-            var sum = new Fraction(3).Plus(new Fraction(4));
-
-            Assert.Equal(7, sum.IntValue());
+            Assert.Equal(7, new Fraction(3).Plus(new Fraction(4)).IntValue());
         }
 
         [Fact]
         public void NegativeInputsAndNegativeOutputs()
         {
-            var sum = new Fraction(-3).Plus(new Fraction(1));
-
-            Assert.Equal(-2, sum.IntValue());
+            Assert.Equal(-2, new Fraction(-3).Plus(new Fraction(1)).IntValue());
         }
-
     }
 }
